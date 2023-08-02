a55c62e5c6a6d3cd29516feac42f392721f7c2db
calvinchang <q20274982@gmail.com>
2023-08-02 16:29:33 +0800

    feat: add blocto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, bloctoWallet, metamaskWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
@@ -10,8 +10,10 @@ const activeChain = "ethereum";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
+      supportedWallets={[
+        bloctoWallet()
+      ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
